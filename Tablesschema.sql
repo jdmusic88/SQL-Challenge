1dcd9309e5647cba7bfb4292c816b564714c379d
James Martin <57886859+jdmusic88@users.noreply.github.com>
2020-02-09 20:42:43 -0500

    Update and rename Tablesschema .sql to Tablesschema.sql

--- Tablesschema.sql
+++ Tablesschema.sql
@@ -84,14 +84,4 @@ ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
 
--- Query Each Table to confirm data
---------------------------------
-
-SELECT * FROM departments;
-SELECT * FROM dept_emp;
-SELECT * FROM dept_manager;
-SELECT * FROM employees;
-SELECT * FROM salaries;
-SELECT * FROM titles;
-
 
